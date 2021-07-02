4227db4fc16b0a5792cec6af42188eb1759bc657
hossein <hossien.alipour@gmail.com>
2021-07-02 03:21:53 +0300

    add try/caatch

--- src/commands/install.js
+++ src/commands/install.js
@@ -1,15 +1,22 @@
 const spawn = require('../spawn');
 const execute = require('../execute')
 module.exports = async function(repos) {
-    let cloneFailed = await require('./clone.js')(repos)
+    try {
+        let cloneFailed = await require('./clone.js')(repos)
 
-    let installFailed = await execute('yarn install', repos)
+        let installFailed = await execute('yarn install', repos)
 
-    let linkFailed = await require('./link.js')(repos)
-    let failed = [...cloneFailed, ...installFailed, ...linkFailed];
+        let linkFailed = await require('./link.js')(repos)
+        let failed = [...cloneFailed, ...installFailed, ...linkFailed];
 
-    let exitCode = spawn('yarn', ['start', '-w'], { cwd: '../CoCreateJS' })
-    if (exitCode !== 0) {
-        failed.push({ name: 'cocreatejs', des: `yarn start failed` })
+        let exitCode = spawn('yarn', ['start', '-w'], { cwd: '../CoCreateJS' })
+        if (exitCode !== 0) {
+            failed.push({ name: 'cocreatejs', des: `yarn start failed` })
+        }
+    } catch (err) {
+        console.error(err);
+        failed.push({ name: 'general', des: err.message })
+
     }
+
 }
