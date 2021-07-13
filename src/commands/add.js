6a44a8c2fdca84c6b821ca212ff5711cf9bb72bf
frank pagan <frank@cocreate.app>
2021-07-13 22:08:54 +0000

    run batch yarn add

--- src/commands/add.js
+++ src/commands/add.js
@@ -3,6 +3,8 @@ const path = require("path")
 const spawn = require('../spawn');
 const colors = require('colors');
 const addMeta = require('../addMeta');
+
+
 module.exports = async function updateYarnLink(repos) {
     let failed = [];
 
@@ -21,8 +23,8 @@ module.exports = async function updateYarnLink(repos) {
     // console.log(repos)
     // return [];
     for (let repo of repos) {
-        await reAdd(repo.deps, repo, failed)
-        await reAdd(repo.devDeps, repo, failed, '-D')
+        await reAdd(repo.deps, repo, failed, '--non-interactive')
+        await reAdd(repo.devDeps, repo, failed, '-D --non-interactive')
     }
     return failed;
 
@@ -30,19 +32,17 @@ module.exports = async function updateYarnLink(repos) {
 
 
 async function reAdd(deps, repo, failed, param = '') {
-    for (let dep of deps) {
-        console.log(`yarn add ${dep}`)
         try {
-
-            let exitCode = await spawn(`yarn`, ['add', ...param && [param], dep], {
+            let packageList = deps.join(' ');
+            let exitCode = await spawn(`yarn`, ['add', ...param && [param], packageList], {
                 cwd: repo.ppath, stdio: 'inherit',
             });
             if (exitCode !== 0) {
                 failed.push({
                     name: repo.name,
-                    des: `yarn add ${param} ${dep}`
+                    des: `yarn add ${param} ${packageList}`
                 })
-                console.error(`${repo.name}: yarn add ${param} ${dep}`.red)
+                console.error(`${repo.name}: yarn add ${param} ${packageList}`.red)
             }
         }
         catch (err) {
@@ -54,4 +54,4 @@ async function reAdd(deps, repo, failed, param = '') {
         }
     }
 
-}
+
