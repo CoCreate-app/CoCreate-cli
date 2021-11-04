ac6cee9a859b92da86bc4d4896d241b1306e7879
Frank pagan <frank@cocreate.app>
2021-11-04 06:07:02 +0000

    fix: update dependencies

--- src/commands/fs/contribution.js
+++ src/commands/fs/contribution.js
@@ -119,6 +119,7 @@ We want this library to be community-driven, and CoCreate led. We need your help
 
 We appreciate your continued support, thank you!
 
+
 # License
 [The MIT License (MIT)](https://github.com/CoCreate-app/CoCreate-${shortName}/blob/master/LICENSE)
 
