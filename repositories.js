ac569409a1a1eaa9d4127b4c96a6eb8a5118c193
Frank Pagan <frank@cocreate.app>
2022-09-30 15:28:49 +0000

    feat: bump.js - to update @cocreate dependency versions in package.json

---
+++ repositories.js
@@ -0,0 +1,409 @@
+module.exports = [
+
+    {
+        'path': '/home/ubuntu/CoCreateServer/CoCreateJS',
+        'repo': 'github.com/CoCreate-app/CoCreateJS.git'
+    },
+    {
+        'path': '/home/ubuntu/CoCreateServer/CoCreateCSS',
+        'repo': 'github.com/CoCreate-app/CoCreateCSS.git'
+    },
+    {
+        'path': '/home/ubuntu/CoCreateServer/CoCreate-admin',
+        'repo': 'github.com/CoCreate-app/CoCreate-admin.git'
+    },
+    {
+        'path': '/home/ubuntu/CoCreateServer/CoCreate-website',
+        'repo': 'github.com/CoCreate-app/CoCreate-website.git'
+    },
+    // {
+    //     'path': '/home/ubuntu/CoCreateServer/CoCreate-website-template',
+    //     'repo': 'github.com/CoCreate-app/CoCreate-website-template.git'
+    // },
+
+    // Components       
+    {
+        'path': '/home/ubuntu/CoCreateServer/CoCreate-components/CoCreate-actions',
+        'repo': 'github.com/CoCreate-app/CoCreate-actions.git'
+    },
+    {
+        'path': '/home/ubuntu/CoCreateServer/CoCreate-components/CoCreate-api',
+        'repo': 'github.com/CoCreate-app/CoCreate-api.git'
+    },
+    {
+        'path': '/home/ubuntu/CoCreateServer/CoCreate-components/CoCreate-attributes',
+        'repo': 'github.com/CoCreate-app/CoCreate-attributes.git'
+    },
+    {
+        'path': '/home/ubuntu/CoCreateServer/CoCreate-components/CoCreate-calculation',
+        'repo': 'github.com/CoCreate-app/CoCreate-calculation.git'
+    },
+    {
+        'path': '/home/ubuntu/CoCreateServer/CoCreate-components/CoCreate-cli',
+        'repo': 'github.com/CoCreate-app/CoCreate-cli.git'
+    },
+    {
+        'path': '/home/ubuntu/CoCreateServer/CoCreate-components/CoCreate-conditional-logic',
+        'repo': 'github.com/CoCreate-app/CoCreate-conditional-logic.git'
+    },
+    {
+        'path': '/home/ubuntu/CoCreateServer/CoCreate-components/CoCreate-crdt',
+        'repo': 'github.com/CoCreate-app/CoCreate-crdt.git',
+    },
+    {
+        'path': '/home/ubuntu/CoCreateServer/CoCreate-components/CoCreate-crud-client',
+        'repo': 'github.com/CoCreate-app/CoCreate-crud-client.git'
+    },
+    {
+        'path': '/home/ubuntu/CoCreateServer/CoCreate-components/CoCreate-css-parser',
+        'repo': 'github.com/CoCreate-app/CoCreate-css-parser.git'
+    },
+    {
+        'path': '/home/ubuntu/CoCreateServer/CoCreate-components/CoCreate-cursors',
+        'repo': 'github.com/CoCreate-app/CoCreate-cursors.git'
+    },
+    {
+        'path': '/home/ubuntu/CoCreateServer/CoCreate-components/CoCreate-dnd',
+        'repo': 'github.com/CoCreate-app/CoCreate-dnd.git'
+    },
+    {
+        'path': '/home/ubuntu/CoCreateServer/CoCreate-components/CoCreate-docs',
+        'repo': 'github.com/CoCreate-app/CoCreate-docs.git'
+    },
+    {
+        'path': '/home/ubuntu/CoCreateServer/CoCreate-components/CoCreate-element-config',
+        'repo': 'github.com/CoCreate-app/CoCreate-element-config.git'
+    },
+    {
+        'path': '/home/ubuntu/CoCreateServer/CoCreate-components/CoCreate-elements',
+        'repo': 'github.com/CoCreate-app/CoCreate-elements.git'
+    },
+    {
+        'path': '/home/ubuntu/CoCreateServer/CoCreate-components/CoCreate-fetch',
+        'repo': 'github.com/CoCreate-app/CoCreate-fetch.git'
+    },
+    {
+        'path': '/home/ubuntu/CoCreateServer/CoCreate-components/CoCreate-filter',
+        'repo': 'github.com/CoCreate-app/CoCreate-filter.git'
+    },
+    {
+        'path': '/home/ubuntu/CoCreateServer/CoCreate-components/CoCreate-floating-label',
+        'repo': 'github.com/CoCreate-app/CoCreate-floating-label.git'
+    },
+    {
+        'path': '/home/ubuntu/CoCreateServer/CoCreate-components/CoCreate-form',
+        'repo': 'github.com/CoCreate-app/CoCreate-form.git'
+    },
+    {
+        'path': '/home/ubuntu/CoCreateServer/CoCreate-components/CoCreate-fullscreen',
+        'repo': 'github.com/CoCreate-app/CoCreate-fullscreen.git'
+    },
+    {
+        'path': '/home/ubuntu/CoCreateServer/CoCreate-components/CoCreate-hosting',
+        'repo': 'github.com/CoCreate-app/CoCreate-hosting.git'
+    },
+    {
+        'path': '/home/ubuntu/CoCreateServer/CoCreate-components/CoCreate-indexeddb',
+        'repo': 'github.com/CoCreate-app/CoCreate-indexeddb.git'
+    },
+    {
+        'path': '/home/ubuntu/CoCreateServer/CoCreate-components/CoCreate-industry',
+        'repo': 'github.com/CoCreate-app/CoCreate-industry.git'
+    },
+    {
+        'path': '/home/ubuntu/CoCreateServer/CoCreate-components/CoCreate-lazy-loader',
+        'repo': 'github.com/CoCreate-app/CoCreate-lazy-loader.git'
+    },
+    {
+        'path': '/home/ubuntu/CoCreateServer/CoCreate-components/CoCreate-local-storage',
+        'repo': 'github.com/CoCreate-app/CoCreate-local-storage.git'
+    },
+    {
+        'path': '/home/ubuntu/CoCreateServer/CoCreate-components/CoCreate-link',
+        'repo': 'github.com/CoCreate-app/CoCreate-link.git'
+    },
+    {
+        'path': '/home/ubuntu/CoCreateServer/CoCreate-components/CoCreate-message-client',
+        'repo': 'github.com/CoCreate-app/CoCreate-message-client.git'
+    },
+    {
+        'path': '/home/ubuntu/CoCreateServer/CoCreate-components/CoCreate-modal',
+        'repo': 'github.com/CoCreate-app/CoCreate-modal.git'
+    },
+    {
+        'path': '/home/ubuntu/CoCreateServer/CoCreate-components/CoCreate-observer',
+        'repo': 'github.com/CoCreate-app/CoCreate-observer.git'
+    },
+    {
+        'path': '/home/ubuntu/CoCreateServer/CoCreate-components/CoCreate-organizations',
+        'repo': 'github.com/CoCreate-app/CoCreate-organizations.git'
+    },
+    {
+        'path': '/home/ubuntu/CoCreateServer/CoCreate-components/CoCreate-overlay-scroll',
+        'repo': 'github.com/CoCreate-app/CoCreate-overlay-scroll.git'
+    },
+    {
+        'path': '/home/ubuntu/CoCreateServer/CoCreate-components/CoCreate-parallax',
+        'repo': 'github.com/CoCreate-app/CoCreate-parallax.git'
+    },
+    {
+        'path': '/home/ubuntu/CoCreateServer/CoCreate-components/CoCreate-pass',
+        'repo': 'github.com/CoCreate-app/CoCreate-pass.git'
+    },
+    {
+        'path': '/home/ubuntu/CoCreateServer/CoCreate-components/CoCreate-position',
+        'repo': 'github.com/CoCreate-app/CoCreate-position.git'
+    },
+    {
+        'path': '/home/ubuntu/CoCreateServer/CoCreate-components/CoCreate-progress',
+        'repo': 'github.com/CoCreate-app/CoCreate-progress.git'
+    },
+    {
+        'path': '/home/ubuntu/CoCreateServer/CoCreate-components/CoCreate-pwa',
+        'repo': 'github.com/CoCreate-app/CoCreate-pwa.git'
+    },
+    {
+        'path': '/home/ubuntu/CoCreateServer/CoCreate-components/CoCreate-random-color',
+        'repo': 'github.com/CoCreate-app/CoCreate-random-color.git'
+    },
+    {
+        'path': '/home/ubuntu/CoCreateServer/CoCreate-components/CoCreate-remove',
+        'repo': 'github.com/CoCreate-app/CoCreate-remove.git'
+    },
+    {
+        'path': '/home/ubuntu/CoCreateServer/CoCreate-components/CoCreate-render',
+        'repo': 'github.com/CoCreate-app/CoCreate-render.git'
+    },
+    {
+        'path': '/home/ubuntu/CoCreateServer/CoCreate-components/CoCreate-render-json',
+        'repo': 'github.com/CoCreate-app/CoCreate-render-json.git'
+    },
+    {
+        'path': '/home/ubuntu/CoCreateServer/CoCreate-components/CoCreate-resize',
+        'repo': 'github.com/CoCreate-app/CoCreate-resize.git'
+    },
+    {
+        'path': '/home/ubuntu/CoCreateServer/CoCreate-components/CoCreate-resize-observer',
+        'repo': 'github.com/CoCreate-app/CoCreate-resize-observer.git'
+    },
+    {
+        'path': '/home/ubuntu/CoCreateServer/CoCreate-components/CoCreate-rich-text',
+        'repo': 'github.com/CoCreate-app/CoCreate-rich-text.git'
+    },
+    {
+        'path': '/home/ubuntu/CoCreateServer/CoCreate-components/CoCreate-room',
+        'repo': 'github.com/CoCreate-app/CoCreate-room.git'
+    },
+    {
+        'path': '/home/ubuntu/CoCreateServer/CoCreate-components/CoCreate-scroll',
+        'repo': 'github.com/CoCreate-app/CoCreate-scroll.git'
+    },
+    {
+        'path': '/home/ubuntu/CoCreateServer/CoCreate-components/CoCreate-search',
+        'repo': 'github.com/CoCreate-app/CoCreate-search.git'
+    },
+    {
+        'path': '/home/ubuntu/CoCreateServer/CoCreate-components/CoCreate-select',
+        'repo': 'github.com/CoCreate-app/CoCreate-select.git'
+    },
+    {
+        'path': '/home/ubuntu/CoCreateServer/CoCreate-components/CoCreate-selected',
+        'repo': 'github.com/CoCreate-app/CoCreate-selected.git'
+    },
+    {
+        'path': '/home/ubuntu/CoCreateServer/CoCreate-components/CoCreate-selection',
+        'repo': 'github.com/CoCreate-app/CoCreate-selection.git'
+    },
+    {
+        'path': '/home/ubuntu/CoCreateServer/CoCreate-components/CoCreate-signup-animation',
+        'repo': 'github.com/CoCreate-app/CoCreate-signup-animation.git'
+    },
+    {
+        'path': '/home/ubuntu/CoCreateServer/CoCreate-components/CoCreate-socket-client',
+        'repo': 'github.com/CoCreate-app/CoCreate-socket-client.git'
+    },
+    {
+        'path': '/home/ubuntu/CoCreateServer/CoCreate-components/CoCreate-social-share',
+        'repo': 'github.com/CoCreate-app/CoCreate-social-share.git'
+    },
+    {
+        'path': '/home/ubuntu/CoCreateServer/CoCreate-components/CoCreate-splitview',
+        'repo': 'github.com/CoCreate-app/CoCreate-splitview.git'
+    },
+    {
+        'path': '/home/ubuntu/CoCreateServer/CoCreate-components/CoCreate-text',
+        'repo': 'github.com/CoCreate-app/CoCreate-text.git'
+    },
+    {
+        'path': '/home/ubuntu/CoCreateServer/CoCreate-components/CoCreate-toggle',
+        'repo': 'github.com/CoCreate-app/CoCreate-toggle.git'
+    },
+    {
+        'path': '/home/ubuntu/CoCreateServer/CoCreate-components/CoCreate-toolbar',
+        'repo': 'github.com/CoCreate-app/CoCreate-toolbar.git'
+    },
+    {
+        'path': '/home/ubuntu/CoCreateServer/CoCreate-components/CoCreate-unique',
+        'repo': 'github.com/CoCreate-app/CoCreate-unique.git'
+    },
+    {
+        'path': '/home/ubuntu/CoCreateServer/CoCreate-components/CoCreate-users',
+        'repo': 'github.com/CoCreate-app/CoCreate-users.git'
+    },
+    {
+        'path': '/home/ubuntu/CoCreateServer/CoCreate-components/CoCreate-utils',
+        'repo': 'github.com/CoCreate-app/CoCreate-utils.git'
+    },
+    {
+        'path': '/home/ubuntu/CoCreateServer/CoCreate-components/CoCreate-uuid',
+        'repo': 'github.com/CoCreate-app/CoCreate-uuid.git'
+    },
+    {
+        'path': '/home/ubuntu/CoCreateServer/CoCreate-components/CoCreate-validation',
+        'repo': 'github.com/CoCreate-app/CoCreate-validation.git'
+    },
+    {
+        'path': '/home/ubuntu/CoCreateServer/CoCreate-components/CoCreate-vdom',
+        'repo': 'github.com/CoCreate-app/CoCreate-vdom.git'
+    },
+
+    // Modules        
+    {
+        'path': '/home/ubuntu/CoCreateServer/CoCreate-apps/CoCreate-builder',
+        'repo': 'github.com/CoCreate-app/CoCreate-builder.git'
+    },
+    // {
+    //     'path':'../CoCreate-apps/CoCreate-calendar',
+    //     'repo' : 'github.com/CoCreate-app/CoCreate-calendar.git'
+    // },
+    // {
+    //     'path':'../CoCreate-apps/CoCreate-checkout',
+    //     'repo' : 'github.com/CoCreate-app/CoCreate-checkout.git'
+    // },
+    {
+        'path': '/home/ubuntu/CoCreateServer/CoCreate-apps/CoCreate-dashboard',
+        'repo': 'github.com/CoCreate-app/CoCreate-dashboard.git'
+    },
+    {
+        'path': '/home/ubuntu/CoCreateServer/CoCreate-apps/CoCreate-datatable',
+        'repo': 'github.com/CoCreate-app/CoCreate-datatable.git'
+    },
+    // {
+    //     'path':'../CoCreate-apps/CoCreate-email',
+    //     'repo' : 'github.com/CoCreate-app/CoCreate-email.git'
+    // },
+    {
+        'path': '/home/ubuntu/CoCreateServer/CoCreate-apps/CoCreate-kanban',
+        'repo': 'github.com/CoCreate-app/CoCreate-kanban.git'
+    },
+    {
+        'path': '/home/ubuntu/CoCreateServer/CoCreate-apps/CoCreate-sandbox',
+        'repo': 'github.com/CoCreate-app/CoCreate-sandbox.git'
+    },
+
+    // Plugins        
+    {
+        'path': '/home/ubuntu/CoCreateServer/CoCreate-plugins/CoCreate-charts',
+        'repo': 'github.com/CoCreate-app/CoCreate-charts.git'
+    },
+    // {
+    //     'path': '/home/ubuntu/CoCreateServer/CoCreate-plugins/CoCreate-codemirror',
+    //     'repo': 'github.com/CoCreate-app/CoCreate-codemirror.git'
+    // },
+    {
+        'path': '/home/ubuntu/CoCreateServer/CoCreate-plugins/CoCreate-croppie',
+        'repo': 'github.com/CoCreate-app/CoCreate-croppie.git'
+    },
+    {
+        'path': '/home/ubuntu/CoCreateServer/CoCreate-plugins/CoCreate-domain',
+        'repo': 'github.com/CoCreate-app/CoCreate-domain.git'
+    },
+    {
+        'path': '/home/ubuntu/CoCreateServer/CoCreate-plugins/CoCreate-fullcalendar',
+        'repo': 'github.com/CoCreate-app/CoCreate-fullcalendar.git'
+    },
+    {
+        'path': '/home/ubuntu/CoCreateServer/CoCreate-plugins/CoCreate-facebook',
+        'repo': 'github.com/CoCreate-app/CoCreate-facebook.git'
+    },
+    {
+        'path': '/home/ubuntu/CoCreateServer/CoCreate-plugins/CoCreate-font-awesome',
+        'repo': 'github.com/CoCreate-app/CoCreate-font-awesome.git'
+    },
+    {
+        'path': '/home/ubuntu/CoCreateServer/CoCreate-plugins/CoCreate-google-maps',
+        'repo': 'github.com/CoCreate-app/CoCreate-google-maps.git'
+    },
+    {
+        'path': '/home/ubuntu/CoCreateServer/CoCreate-plugins/CoCreate-google-auth',
+        'repo': 'github.com/CoCreate-app/CoCreate-google-auth.git'
+    },
+    {
+        'path': '/home/ubuntu/CoCreateServer/CoCreate-plugins/CoCreate-instagram',
+        'repo': 'github.com/CoCreate-app/CoCreate-instagram.git'
+    },
+    {
+        'path': '/home/ubuntu/CoCreateServer/CoCreate-plugins/CoCreate-lighthouse',
+        'repo': 'github.com/CoCreate-app/CoCreate-lighthouse.git'
+    },
+    {
+        'path': '/home/ubuntu/CoCreateServer/CoCreate-plugins/CoCreate-linkedin',
+        'repo': 'github.com/CoCreate-app/CoCreate-linkedin.git'
+    },
+    // {
+    //     'path': '/home/ubuntu/CoCreateServer/CoCreate-plugins/CoCreate-monaco',
+    //     'repo': 'github.com/CoCreate-app/CoCreate-monaco.git'
+    // },
+    {
+        'path': '/home/ubuntu/CoCreateServer/CoCreate-plugins/CoCreate-pickr',
+        'repo': 'github.com/CoCreate-app/CoCreate-pickr.git'
+    },
+    {
+        'path': '/home/ubuntu/CoCreateServer/CoCreate-plugins/CoCreate-pinterest',
+        'repo': 'github.com/CoCreate-app/CoCreate-pinterest.git'
+    },
+    {
+        'path': '/home/ubuntu/CoCreateServer/CoCreate-plugins/CoCreate-plaid',
+        'repo': 'github.com/CoCreate-app/CoCreate-plaid.git'
+    },
+    {
+        'path': '/home/ubuntu/CoCreateServer/CoCreate-plugins/CoCreate-prism',
+        'repo': 'github.com/CoCreate-app/CoCreate-prism.git'
+    },
+    // {
+    //     'path': '/home/ubuntu/CoCreateServer/CoCreate-plugins/CoCreate-quill',
+    //     'repo': 'github.com/CoCreate-app/CoCreate-quill.git'
+    // },
+    {
+        'path': '/home/ubuntu/CoCreateServer/CoCreate-plugins/CoCreate-s3',
+        'repo': 'github.com/CoCreate-app/CoCreate-s3.git'
+    },
+    {
+        'path': '/home/ubuntu/CoCreateServer/CoCreate-plugins/CoCreate-sendgrid',
+        'repo': 'github.com/CoCreate-app/CoCreate-sendgrid.git'
+    },
+    {
+        'path': '/home/ubuntu/CoCreateServer/CoCreate-plugins/CoCreate-shipengine',
+        'repo': 'github.com/CoCreate-app/CoCreate-shipengine.git'
+    },
+    {
+        'path': '/home/ubuntu/CoCreateServer/CoCreate-plugins/CoCreate-stripe',
+        'repo': 'github.com/CoCreate-app/CoCreate-stripe.git'
+    },
+    {
+        'path': '/home/ubuntu/CoCreateServer/CoCreate-plugins/CoCreate-tui-image-editor',
+        'repo': 'github.com/CoCreate-app/CoCreate-tui-image-editor.git'
+    },
+    {
+        'path': '/home/ubuntu/CoCreateServer/CoCreate-plugins/CoCreate-twilio',
+        'repo': 'github.com/CoCreate-app/CoCreate-twilio.git'
+    },
+    {
+        'path': '/home/ubuntu/CoCreateServer/CoCreate-plugins/CoCreate-twitter',
+        'repo': 'github.com/CoCreate-app/CoCreate-twitter.git'
+    },
+    {
+        'path': '/home/ubuntu/CoCreateServer/CoCreate-plugins/CoCreate-uppy',
+        'repo': 'github.com/CoCreate-app/CoCreate-uppy.git'
+    }
+];
---
+++ src/commands/fs/bump.js
@@ -0,0 +1,78 @@
+
+let fs = require('fs');
+const prettier = require("prettier");
+let list = require('../../../repositories.js');
+const path = require("path")
+
+let CoCreateJsPath = path.resolve('../CoCreateJS');
+
+let pathList = list.map(o => o.path)
+let nameList = pathList.map(fn => path.basename(fn).toLowerCase());
+let item = {}
+
+
+// (async() => {
+
+//     for (let [index, name] of nameList.entries()) {
+//         getVersions(pathList[index] + '/package.json', name).
+//     }
+//     // console.log('bump versions', item)
+//     // for (let [index, name] of nameList.entries()) {
+//     //     await bumpVersion(pathList[index] + '/package.json', name)
+//     // }
+
+
+// })();
+
+async function run() {
+    for (let [index, name] of nameList.entries()) {
+       getVersions(pathList[index] + '/package.json', `@${name}`)
+    }
+    console.log('bump versions', item)
+
+    for (let [index, name] of nameList.entries()) {
+        await bumpVersion(pathList[index] + '/package.json', name)
+    }
+
+    console.log('completed')
+    // process.exit()  
+}
+
+function getVersions(path, name) {
+    if(!fs.existsSync(path))
+        return console.error('path doesn\'t exist:', path)
+    let object = require(path)
+    if (object.name && object.version) {
+        item[object.name] = object.version
+    }
+}
+
+function bumpVersion(filePath, name) {
+    if(!fs.existsSync(filePath))
+        return console.error('path doesn\'t exist:', path)
+    let object = require(filePath)
+    let newObject = {...object}
+
+    if (!object.dependencies)
+        return console.log(name, 'not updated')
+    else {
+        for (const name of Object.keys(object.dependencies)) {
+            if (item[name]) {
+                newObject.dependencies[name] = item[name]
+            }
+        }
+
+        let str = JSON.stringify(object)
+        let formated = prettier.format(str, { semi: false, parser: "json" });
+
+        filePath = filePath.replace('/package.json', '')
+        let Path = path.resolve(filePath, 'package.json')
+        if (fs.existsSync(Path)){
+            fs.unlinkSync(Path)
+        }
+
+        fs.writeFileSync(Path, formated)
+    }
+}
+
+run()
