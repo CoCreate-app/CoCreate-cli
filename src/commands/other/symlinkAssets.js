44f9f7830faba3f41b07032c89890c65f54f4d71
frankpagan <frank@cocreate.app>
2023-05-01 00:54:49 -0600

    testing symlink

---
+++ src/commands/other/symlinkAssets.js
@@ -0,0 +1,26 @@
+const fs = require('fs')
+
+function createSymlink(dir, dest){
+    if (!fs.existsSync(dir)) {
+        console.log('does not exist')
+        return
+    } 
+
+    if (fs.existsSync(dest))
+        fs.unlinkSync(dest)
+
+    fs.symlink( dir, dest, 'dir', (err) => {
+        if (err)
+          console.log(err);
+        else {
+          console.log("symlink added");
+        }    
+    })
+
+}
+
+
+// createSymlink('./assets', '../../assets')
+createSymlink('./manifest.json', '../../manifest.json')
+createSymlink('./sw.js', '../../sw.js')
+createSymlink('./offline.html', '../../offline.html')
