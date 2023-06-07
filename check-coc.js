cad848bfefe2f66fb323eca3eac292d9547e6cc6
frankpagan <frank@cocreate.app>
2023-06-06 19:11:00 -0600

    feat: Install "coc" globally if not already installed
    This commit adds a check for the global installation of the "coc" command and installs it globally if not already installed. In case of failure while installing "coc" globally, an error message will be displayed.

---
+++ check-coc.js
@@ -0,0 +1,16 @@
+const { execSync } = require('child_process');
+
+try {
+    // Try to run "coc" command
+    execSync('coc --version', { stdio: 'ignore' });
+    console.log('"coc" is already installed globally.');
+} catch (error) {
+    // If "coc" command does not exist, install it globally
+    console.log('"coc" command not found. Installing globally...');
+    try {
+        execSync('npm install -g coc', { stdio: 'inherit' });
+        console.log('"coc" has been installed globally.');
+    } catch (error) {
+        console.error('Failed to install "coc" globally:', error);
+    }
+}
