64a53630e0d26843ac0e951a89f496da72e344a9
frankpagan <frank@cocreate.app>
2023-08-20 15:00:52 -0600

    feat: option support to define a custom configPath

--- src/commands/upload.js
+++ src/commands/upload.js
@@ -1,5 +1,42 @@
 const file = require('@cocreate/file')
+const path = require('path');
+const fs = require('fs');
 
 module.exports = async function upload(repos, args) {
-    await file()
+    let CoCreateConfig
+    if (!args.length) {
+        let configFile = path.resolve(process.cwd(), 'CoCreate.config.js');
+        if (!CoCreateConfig && fs.existsSync(configFile)) {
+            CoCreateConfig = require(configFile);
+        } else {
+            console.log('CoCreate.config.js could not be found.')
+            process.exit()
+        }
+
+        await file(CoCreateConfig)
+
+    } else {
+        for (let arg of args) {
+            try {
+                CoCreateConfig = JSON.parse(arg)
+            } catch (error) { }
+
+
+            if (!CoCreateConfig) {
+                try {
+                    let configPath = path.resolve(process.cwd(), arg)
+                    if (fs.existsSync(configPath)) {
+                        CoCreateConfig = require(configPath);
+                        if (CoCreateConfig)
+                            await file(CoCreateConfig, configPath)
+
+                    } else {
+                        console.log(arg + ' could not be found.')
+                        process.exit()
+                    }
+
+                } catch (error) { }
+            }
+        }
+    }
 }
