5b505cd29ac917c7297725d30640c1fa48cfffc5
frankpagan <frank@cocreate.app>
2023-08-20 21:28:44 -0600

    feat: create a spawn for letsencrypt and extract the txt value that needs to be added in the dns

--- src/commands/other/certBotWildcard.js
+++ src/commands/other/certBotWildcard.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 const { spawn } = require('child_process');
 
 function runCertbot() {
@@ -9,24 +11,35 @@ function runCertbot() {
             '-d', '*.cocreate.zone',
             '-d', 'cocreate.zone',
             '--agree-tos',
-            '-m', 'admin@cocreate.app',
+            '-m', 'admin@cocreate.zone',
             '--preferred-challenges', 'dns-01',
             '--server', 'https://acme-v02.api.letsencrypt.org/directory'
         ]);
 
-        let promptsCount = 0;
+        // let promptsCount = 0;
 
         certbotProcess.stdout.on('data', (data) => {
             const output = data.toString();
-            if (output.includes('Enter your response here')) {
-                promptsCount++;
-                if (promptsCount === 1) {
-                    certbotProcess.stdin.write('Your first response\n');
-                } else if (promptsCount === 2) {
-                    certbotProcess.stdin.write('Your second response\n');
-                } else if (promptsCount === 3) {
-                    certbotProcess.stdin.write('Your third response\n');
+            if (output.includes('Please deploy a DNS TXT record under the name:')) {
+                const namePattern = /Please deploy a DNS TXT record under the name:\s+([\w\.-]+)\./;
+                const valuePattern = /with the following value:\s+([\w\d]+)\s+/;
+
+                const nameMatch = output.match(namePattern);
+                const valueMatch = output.match(valuePattern);
+
+                if (nameMatch && valueMatch) {
+                    const name = nameMatch[1];
+                    const value = valueMatch[1];
+
+                    console.log("Name:", name);
+                    console.log("Value:", value);
+                } else {
+                    console.log("Name or value not found in the text.");
                 }
+                // TODO: send name and value to client and wait for clients response before continuing
+                certbotProcess.stdin.write('\n');
+            } else if (output.includes('Press Enter to Continue')) {
+                certbotProcess.stdin.write('\n');
             }
         });
 
