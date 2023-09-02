b09bb2f365e95bc054a24ea0920cad0048dbcefc
frankpagan <frank@cocreate.app>
2023-09-02 12:14:30 -0600

    added symlinkDist

---
+++ src/commands/other/symlinkDist.js
@@ -0,0 +1,36 @@
+const fs = require('fs')
+const path = require('path')
+
+function symlink(dir, dest, option) {
+    if (!fs.existsSync(dir)) {
+        return console.log(dir, 'does not exist')
+    } else
+        dir = path.resolve(dir)
+
+    if (fs.existsSync(dest))
+        fs.rm(dest, { recursive: true, force: true }, function (err) {
+            if (err) {
+                console.log('failed');
+            }
+            createSymlink(dir, dest, option)
+        });
+    else
+        createSymlink(dir, dest, option)
+}
+
+function createSymlink(dir, dest, option) {
+    dest = path.resolve(dest)
+
+    fs.symlink(dir, dest, option, (err) => {
+        console.log(dir, dest, option)
+        if (err)
+            console.log(err);
+        else {
+            console.log("symlink added");
+        }
+
+    })
+
+}
+
+symlink('./dist', '../dist', 'dir')
