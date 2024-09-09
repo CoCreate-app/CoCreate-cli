76dca35470b20351dfff6105ce8495cb8d787128
frankpagan <frank@cocreate.app>
2024-09-09 19:34:56 +0000

    feat: repo url will add https if not present

--- src/commands/clone.js
+++ src/commands/clone.js
@@ -12,7 +12,10 @@ module.exports = async function gitClone(repos, args) {
             if (!fs.existsSync(repos[i].directory))
                 fs.mkdirSync(repos[i].directory);
 
-            let exitCode = await spawn('git', ['clone', `https://${repos[i].repo}`], { stdio: 'inherit', cwd: repos[i].directory })
+            if (!repos[i].repo.startsWith('http://') && !repos[i].repo.startsWith('https://'))
+                repos[i].repo = 'https://' + repos[i].repo
+
+            let exitCode = await spawn('git', ['clone', `${repos[i].repo}`], { stdio: 'inherit', cwd: repos[i].directory })
             if (exitCode !== 0) {
                 failed.push({ name: repos[i].name, des: `cloning failed` })
             }
