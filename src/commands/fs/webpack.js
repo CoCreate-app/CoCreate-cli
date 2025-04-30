4743b98f0f7d2c67678d487f5cbfd42fc5f3ea63
frankpagan <frank@cocreate.app>
2025-04-30 01:28:30 -0600

    fix: improved handeling of webpack config generation

--- src/commands/fs/webpack.js
+++ src/commands/fs/webpack.js
@@ -1,191 +1,185 @@
-let cdnUrl = "https://server.cocreate.app/";
+const fs = require("fs");
+const path = require("path");
+const { execSync } = require("child_process");
 
+module.exports = async function generateWebpackConfigs(repos, args) {
+  console.log("Generating webpack configs for specified repositories...");
 
+  if (!Array.isArray(repos) || repos.length === 0) {
+    console.warn("No repositories specified.");
+    return [];
+  }
 
-let glob = require("glob");
-let fs = require('fs');
-const prettier = require("prettier");
-const path = require("path")
+  const failed = [];
 
-function globUpdater(er, files) {
-  if (er)
-    console.log(files, 'glob resolving issue')
-  else
-    files.forEach(filename => update(filename))
+  for (const repo of repos) {
+    if (!repo || typeof repo.path !== "string") {
+      repo.error = "Invalid repository data provided";
+      failed.push(repo);
+      continue;
+    }
+
+    const result = updateWebpackConfigForModule(repo);
+    if (!result) {
+      repo.error = `${repo.path}/webpack.config.js`;
+      failed.push(repo);
+    } else {
+      console.log(`Generated: ${repo.path}/webpack.config.js`);
+    }
+  }
+
+  console.log(`\nWebpack config generation process finished.`);
+
+  return failed;
+};
+
+function updateWebpackConfigForModule(repo) {
+  try {
+    repo.path = path.resolve(process.cwd(), repo.path);
+
+    if (!fs.existsSync(repo.path) || !fs.lstatSync(repo.path).isDirectory()) {
+      repo.error = `The path "${repo.path}" does not exist or is not a directory.`;
+      return false;
+    }
+
+    const webpackPath = path.resolve(repo.path, "webpack.config.js");
+    let name = path.basename(repo.path);
+
+    if (!fs.existsSync(path.resolve(repo.path, repo.entry))) {
+      repo.error = `Entry file ${repo.entry} does not exist in ${repo.path}`;
+      return false;
+    }
+
+    let componentName = toCamelCase(name);
+    if (componentName === componentName.toUpperCase()) {
+      componentName = componentName.toLowerCase();
+    } else {
+      componentName = lowerCaseFirstChar(componentName);
+    }
+
+    let hasTemplate = false;
+    if (fs.existsSync(path.resolve(repo.path, "./src/index.html"))) {
+      hasTemplate = true;
+    }
+    let fileContent = `
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { EsbuildPlugin } = require("esbuild-loader");
+const { FileUploader } = require("@cocreate/webpack");
+${
+  hasTemplate ? 'const HtmlWebpackPlugin = require("html-webpack-plugin");' : ""
 }
 
-
-
-
-function update(webpackPath) {
-
-
-
-  let dir = path.dirname(webpackPath);
-
-  // component name
-  let name = path.basename(dir);
-  // component entry
-  
-  
-  let entry;
-  
-  if (fs.existsSync( path.resolve(dir, './src/index.js') ))
-    entry = "./src/index.js";
-  else
-    entry = './src/' + name + '.js';
-
-  // get component name in came case "cocreate" less
-  let componentName = toCamelCase(name);
-  if (componentName.startsWith('CoCreate'))
-    componentName = componentName.substring(8);
-
-  if (componentName === componentName.toUpperCase())
-    componentName = componentName.toLowerCase();
-  else
-    componentName = lowerCaseFirstChar(componentName)
-
-
-  // has template to inject script and styles on dev compile
-  let hasTemplate = false;
-  if (fs.existsSync(path.resolve(dir, './src/index.html')))
-    hasTemplate = true;
-  let fileContent = `const path = require("path");
-const TerserPlugin = require("terser-webpack-plugin");
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-let isProduction = process.env.NODE_ENV === "production";
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-${ hasTemplate ? "const HtmlWebpackPlugin = require('html-webpack-plugin');" : ''}
-
-
-
-
-module.exports = {
-  entry: {
-    '${name}': '${entry}'
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: isProduction ? '[name].min.js' : '[name]${hasTemplate ? '[hash]': ''}.js',
-    libraryTarget: 'umd',
-    libraryExport: 'default',
-    library: ${ componentName ? `['CoCreate', '${componentName}']` : 'CoCreate'},
-    globalObject: "this",
-  },
-  
-  
-  plugins: [new CleanWebpackPlugin(),
+module.exports = async (env, argv) => {
+  const isProduction = argv && argv.mode === "production";
+  const config = {
+    entry: {
+      "${name}": "${repo.entry}"
+    },
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: isProduction ? "[name].min.js" : "[name]${
+        hasTemplate ? ".[contenthash]" : ""
+      }.js",
+      libraryExport: "default",
+      library: ["CoCreate", "${componentName}"],
+      clean: true
+    },
+    plugins: [
       new MiniCssExtractPlugin({
-      filename: '[name].css',
-    }),
-    ${ hasTemplate ? `
+        filename: isProduction ? "[name].min.css" : "[name].css",
+      }),
+      new FileUploader(env, argv),
+      ${
+        hasTemplate
+          ? `
       new HtmlWebpackPlugin({
         template: "./src/index.html"
       })
-    ` : ''}
+      `
+          : ""
+      }
     ],
-  // Default mode for Webpack is production.
-  // Depending on mode Webpack will apply different things
-  // on final bundle. For now we don't need production's JavaScript
-  // minifying and other thing so let's set mode to development
-  mode: isProduction ? "production" : "development",
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /(node_modules)/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            plugins: ["@babel/plugin-transform-modules-commonjs"],
+    mode: isProduction ? "production" : "development",
+    devtool: isProduction ? "source-map" : "eval-source-map",
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "esbuild-loader",
+            options: {
+              loader: "js",
+              target: "es2017"
+            }
           },
         },
-      },
-      {
-        test: /\.css$/i,
-        use: [
-          { loader: "style-loader", options: { injectType: "linkTag" } },
-          "file-loader",
-        ],
-      },
-    ],
-  },
-
-
-  // add source map
-  ...(isProduction ? {} : { devtool: 'eval-source-map' }),
-
-optimization: {
-    minimize: true,
-    minimizer: [
-      new TerserPlugin({
-        extractComments: true,
-        // cache: true,
-        parallel: true,
-        // sourceMap: true, // Must be set to true if using source-maps in production
-        terserOptions: {
-          // https://github.com/webpack-contrib/terser-webpack-plugin#terseroptions
-          // extractComments: 'all',
-          compress: {
-            drop_console: true,
-          },
+        {
+          test: /\.css$/i,
+          use: [MiniCssExtractPlugin.loader, "css-loader"]
         },
-      }),
-    ],
+      ],
+    },
+    optimization: {
+      minimize: isProduction,
+      minimizer: [
+        new EsbuildPlugin({
+          target: "es2017",
+          css: true
+        })
+      ],
       splitChunks: {
-        chunks: 'all',
-        minSize: 200,
-        // maxSize: 99999,
-        //minChunks: 1,
-        ${ !hasTemplate ? `    
         cacheGroups: {
           defaultVendors: false
         },
-        ` : ''}
-    
       }
-  },
-}
+    },
+    performance: {
+      hints: isProduction ? "warning" : false
+    }
+  };
+  return config;
+};
 `;
-  let formated = prettier.format(fileContent, { semi: false, parser: "babel" });
-
-  if (fs.existsSync(webpackPath))
-    fs.unlinkSync(webpackPath)
-  fs.writeFileSync(webpackPath, formated)
-  // console.log(fileContent)
-  // process.exit()
-
+    fs.writeFileSync(webpackPath, fileContent);
+    execSync(`npx prettier --write "${webpackPath}"`).toString();
+    return true;
+  } catch (error) {
+    return false;
+  }
 }
-
-
-
-glob("../CoCreate-components/*/webpack.config.js", globUpdater)
-glob("../CoCreate-apps/*/webpack.config.js", globUpdater)
-glob("../CoCreate-plugins/*/webpack.config.js", globUpdater)
-glob("../CoCreateCSS/webpack.config.js", globUpdater)
-
-
-
-
 
 function toCamelCase(str) {
   let index = 0;
+  str = str.replace(/^CoCreate-/, "");
   do {
     index = str.indexOf("-", index);
     if (index !== -1) {
       let t = str.substring(0, index);
-      t += String.fromCharCode(str.charCodeAt(index + 1) - 32);
-      t += str.substring(index + 2);
-      str = t;
-    }
-    else break;
+      if (
+        index + 1 < str.length &&
+        str[index + 1] >= "a" &&
+        str[index + 1] <= "z"
+      ) {
+        t += String.fromCharCode(str.charCodeAt(index + 1) - 32);
+        t += str.substring(index + 2);
+        str = t;
+      } else if (index + 1 < str.length) {
+        t += str.substring(index + 1);
+        str = t;
+      } else {
+        str = t;
+      }
+    } else break;
   } while (true);
   return str;
 }
 
 function lowerCaseFirstChar(str) {
-  return String.fromCharCode(str.charCodeAt(0) + 32) + str.substring(1);
+  if (!str) return str;
+  if (str.charCodeAt(0) >= 65 && str.charCodeAt(0) <= 90) {
+    return String.fromCharCode(str.charCodeAt(0) + 32) + str.substring(1);
+  }
+  return str;
 }
-
-
-//
